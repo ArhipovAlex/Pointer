48ab5b85848157c394afaf58732b5ad776a76c87
Sania <Sania@DESKTOP-3HL58UN>
2023-07-11 21:15:20 +0800

    Add project files.

---
+++ TransferParameters/main.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+void Exchange(int a, int b);
+
+void main() {
+	setlocale(LC_ALL, "rus");
+	int a = 2, b = 3;
+	cout << a << "\t" << b << endl;
+	Exchange(a, b);
+	cout << a << "\t" << b << endl;
+}
+
+void Exchange(int a, int b) {
+	int buff = a;
+	a = b;
+	b = buff;
+}
