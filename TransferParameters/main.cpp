2bed8889d878a6ac2befcfeb9e517fffc5bbaa04
Sania <Sania@DESKTOP-3HL58UN>
2023-07-11 21:32:52 +0800

    transfer parameters DONE

--- TransferParameters/main.cpp
+++ TransferParameters/main.cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
 
-void Exchange(int a, int b);
+void Exchange(int* a, int* b);
 
 void main() {
 	setlocale(LC_ALL, "rus");
 	int a = 2, b = 3;
 	cout << a << "\t" << b << endl;
-	Exchange(a, b);
+	Exchange(&a, &b);
 	cout << a << "\t" << b << endl;
 }
 
-void Exchange(int a, int b) {
-	int buff = a;
-	a = b;
-	b = buff;
+void Exchange(int* a, int* b) {
+	int buff = *a;
+	*a = *b;
+	*b = buff;
 }
